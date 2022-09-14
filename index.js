f1bfb0ff21228c324cff0dd41cb2cf598bdd57e5
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2022-09-15 01:16:10 +0530

    Updated the content of all files to Single JSON Object

--- index.js
+++ index.js
@@ -12,6 +12,62 @@ app.get("/",(req,res)=>{
     
 })
 
+app.get("/storing",(req,res)=>{
+  
+    function readFiles(dirname, onFileContent, onError) {
+      
+      //Function to read directory for filenames
+      fs.readdir('./logs', function(err, filenames) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        console.log("Files : \n",filenames)
+  
+        // Traverse the Logs directory and read files inside the directory and store to some Json Object 
+        filenames.forEach(function(filename) {
+          //console.log(dirname + filename)
+          fs.readFile("logs\\" + filename, 'utf-8', function(err, content, onError) {
+            if (err) {
+              console.log(err)
+              return;
+            }
+            //console.log(content)
+            fs.readFile("logs\\" + filename, 'utf-8', (err, data) => {
+                if (err) throw err;
+            
+                //Convert Content to String Format 
+                let text = data.toString();
+            
+                // First Line of Data is URL do extract URL from 1st Line 
+                var firstLine = text.split("\n").shift(); // first line
+            
+                // Removed #URL from !st Line to add to JSON
+                let EditedURL = firstLine.replace("#URL => ", "");
+                
+                //Extract all Content Except First Line 
+                var linesExceptFirst = text.split("\n").slice(1).join("\n");
+                
+                //Remove all Line Break and Make multi-Line content to single Line Content
+                let SingleLineData = linesExceptFirst.replace(/(\r\n|\n|\r)/gm, "");
+            
+                // Createad Json Object to append to final File
+                const jsonData = {
+                  url: EditedURL,
+                  data: SingleLineData,
+                };
+            
+                console.log(jsonData);
+            
+              });
+          });
+        });
+      });
+    
+    }
+    readFiles()
+  })
+
 app.get("/json", (req, res) => {
 
 
