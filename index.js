e6a0c4d63d46512e9441b99c6405d461c50400bf
Abhijeet Thorat <abhijeett@gist.cdacitpark.in>
2022-09-15 11:27:35 +0530

    Updated Formating and Write to File

--- index.js
+++ index.js
@@ -8,70 +8,55 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/storing",(req,res)=>{
-  
-    function readFiles(dirname, onFileContent, onError) {
-      
-      //Function to read directory for filenames
-      fs.readdir('./logs', function(err, filenames) {
-        if (err) {
-          onError(err);
-          return;
-        }
-        // console.log("Files : \n",filenames)
-  
-        // Traverse the Logs directory and read files inside the directory and store to some Json Object 
-        filenames.forEach(function(filename) {
-          //console.log(dirname + filename)
-          fs.readFile("logs\\" + filename, 'utf-8', function(err, content, onError) {
-            if (err) {
-              console.log(err)
-              return;
-            }
-            //console.log(content)
-            fs.readFile("logs\\" + filename, 'utf-8', (err, data) => {
-                if (err) throw err;
-            
-                //Convert Content to String Format 
-                let text = data.toString();
-            
-                // First Line of Data is URL do extract URL from 1st Line 
-                var firstLine = text.split("\n").shift(); // first line
-            
-                // Removed #URL from !st Line to add to JSON
-                let EditedURL = firstLine.replace("#URL => ", "");
-                
-                //Extract all Content Except First Line 
-                var linesExceptFirst = text.split("\n").slice(1).join("\n");
-                
-                //Remove all Line Break and Make multi-Line content to single Line Content
-                let SingleLineData = linesExceptFirst.replace(/(\r\n|\n|\r)/gm, "");
-            
-                // Createad Json Object to append to final File
-                const jsonData = {
-                  url: EditedURL,
-                  data: SingleLineData,
-                };
-            
-                //console.log(jsonData);
-                return jsonData;
-              });
-          });
+app.get("/storing", (req, res) => {
+  function readFiles(callbackFunc) {
+    //Function to read directory for filenames
+    fs.readdir("./logs", function (err, filenames) {
+      if (err) {
+        // onError(err);
+        return;
+      }
+
+      const data1 = [];
+      // Traverse the Logs directory and read files inside the directory and store to some Json Object
+      filenames.forEach(function (filename) {
+        //console.log(content)
+        fs.readFile("logs\\" + filename, "utf-8", (err, data) => {
+          if (err) throw err;
+
+          //Convert Content to String Format
+          let text = data.toString();
+
+          // First Line of Data is URL do extract URL from 1st Line
+          var firstLine = text.split("\r\n").shift(); // first line
+
+          // Removed #URL from !st Line to add to JSON
+          let EditedURL = firstLine.replace("#URL => ", "");
+
+          //Extract all Content Except First Line
+          var linesExceptFirst = text.split("\n").slice(1).join("\n");
+
+          //Remove all Line Break and Make multi-Line content to single Line Content
+          let SingleLineData = linesExceptFirst.replace(/(\r\n|\n|\r)/gm, " ");
+
+          // Createad Json Object to append to final File
+          const jsonData = {
+            url: EditedURL,
+            data: SingleLineData,
+          };
+          data1.push(jsonData);
+          callbackFunc(data1);
         });
       });
-    }
-    let JSONdata = readFiles()
+    });
+  }
+  function callbackFunc(result) {
+    console.log("Result ", result);
 
-    console.log(JSONdata)
+  }
+  readFiles(callbackFunc);
+});
 
-  //   fs.writeFile('Output.txt', data, (err) => {
-      
-  //     // In case of a error throw err.
-  //     if (err) throw err;
-  // })
-  })
-
-
-app.listen('3001',()=>{
-    console.log("Listing at port 3001")
-})
+app.listen("3001", () => {
+  console.log("Listing at port 3001");
+});
